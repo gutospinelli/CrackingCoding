006bf5e6508c741dafb8d968fc2afdc7bf93da45
Augusto Spinelli <augusto.spinelli@rio.rj.gov.br>
2019-06-12 12:44:09 -0300

    MaxMin Riddle answer from geeksForGeeks

--- CrackingCoding/Hackerrank/StacksAndQueues.cs
+++ CrackingCoding/Hackerrank/StacksAndQueues.cs
@@ -109,7 +109,110 @@ namespace CrackingCoding
 
         //Min Max Riddle
 
-        public static long[] riddle(long[] arr) {
+        public static long[] riddle(long[] arr)
+        {
+            //Size of the array
+            int n = arr.Length;
+
+            // Used to find previous and next smaller  
+            Stack<int> s = new Stack<int>(); 
+  
+            // Arrays to store previous  
+            // and next smaller  
+            long[] left = new long[n + 1]; 
+            long[] right = new long[n + 1]; 
+  
+            // Initialize elements of left[]  
+            // and right[]  
+            for (long i = 0; i < n; i++) 
+            { 
+                left[i] = -1; 
+                right[i] = n; 
+            } 
+  
+            // Fill elements of left[] using logic discussed on  
+            // https://www.geeksforgeeks.org/next-greater-element/  
+            for (int i = 0; i < n; i++) 
+            { 
+                while (s.Count > 0 &&  
+                       arr[s.Peek()] >= arr[i]) 
+                { 
+                    s.Pop(); 
+                } 
+  
+                if (s.Count > 0) 
+                { 
+                    left[i] = s.Peek(); 
+                } 
+  
+                s.Push(i); 
+            } 
+  
+            // Empty the stack as stack is going  
+            // to be used for right[]  
+            while (s.Count > 0) 
+            { 
+                s.Pop(); 
+            } 
+  
+            // Fill elements of right[] using 
+            // same logic  
+            for (int i = n - 1 ; i >= 0 ; i--) 
+            { 
+                while (s.Count > 0 &&  
+                       arr[s.Peek()] >= arr[i]) 
+                { 
+                    s.Pop(); 
+                } 
+  
+                if (s.Count > 0) 
+                { 
+                    right[i] = s.Peek(); 
+                } 
+  
+                s.Push(i); 
+            } 
+  
+            // Create and initialize answer array  
+            long[] ans = new long[n + 1]; 
+            for (int i = 0; i <= n; i++) 
+            { 
+                ans[i] = 0; 
+            } 
+  
+            // Fill answer array by comparing 
+            // minimums of all lengths computed  
+            // using left[] and right[]  
+            for (int i = 0; i < n; i++) 
+            { 
+                // length of the interval  
+                long len = right[i] - left[i] - 1; 
+  
+                // arr[i] is a possible answer for  
+                // this length 'len' interval, check x 
+                // if arr[i] is more than max for 'len'  
+                ans[len] = Math.Max(ans[len], arr[i]); 
+            } 
+  
+            // Some entries in ans[] may not be  
+            // filled yet. Fill them by taking  
+            // values from right side of ans[]  
+            for (int i = n - 1; i >= 1; i--) 
+            { 
+                ans[i] = Math.Max(ans[i], ans[i + 1]); 
+            }
+
+            // Print the result
+            long[] ret = new long[n]; 
+            for (int i = 1; i <= n; i++)
+            {
+                //Console.Write(ans[i] + " ");
+                ret[i-1] = ans[i];
+            }
+            return ret;
+        } 
+
+        public static long[] riddleNaive(long[] arr) {
 
             long[] ret = new long[arr.Length];
             int retIndex = 0;
